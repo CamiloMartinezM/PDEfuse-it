e6794853bc4a08f9077a7a17b1a1ed5da330e4c0
Kanakanajm <kanakanajm@outlook.com>
2024-02-21 17:26:39 +0100

    Add vector-based shape and rotation

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,22 +1,42 @@
 import { useEffect, useRef } from "react";
 import PageLayout from "../components/page-layout";
 import "./home-page.css";
-import { arrow } from "../utils/2d/shapes";
+import { Square, getPath, getRotationMatrix, transformShape } from "../utils/2d/shapes";
 
 function draw(ctx: CanvasRenderingContext2D) {
-    for (let i = 0; i < 6; i++) {
-        for (let j = 0; j < 6; j++) {
+    // blue rect
+    // clear the canvas
+    ctx.clearRect(0, 0, 150, 150);
+    const time = new Date();
 
-            const degree = 45; // change this
-            
-            const arrowPath = arrow(25 * i, 25 * j);
-            ctx.save();
-            ctx.rotate((degree * Math.PI) / 180); // Rotate
-            ctx.fill(arrowPath);
-            ctx.restore();
-        }
-    }
-}
+    const angle =((2 * Math.PI) / 6) * time.getSeconds() +
+    ((2 * Math.PI) / 6000) * time.getMilliseconds();
+
+    const square = new Square(25);
+    const rotatedSquare = transformShape(getRotationMatrix(angle), square);
+    ctx.fill(getPath(75, 75, rotatedSquare));
+
+    
+    // grey rect
+    // ctx.fillStyle = "#4D4E53";
+    // ctx.fillRect(30, 30, 100, 100);
+    // ctx.restore();
+  
+    // // right rectangles, rotate from rectangle center
+    // // draw blue rect
+    // ctx.fillStyle = "#0095DD";
+    // ctx.fillRect(150, 30, 100, 100);
+  
+    // ctx.translate(200, 80); // translate to rectangle center
+    // // x = x + 0.5 * width
+    // // y = y + 0.5 * height
+    // ctx.rotate((Math.PI / 180) * 25); // rotate
+    // ctx.translate(-200, -80); // translate back
+  
+    // // draw grey rect
+    // ctx.fillStyle = "#4D4E53";
+    // ctx.fillRect(150, 30, 100, 100);
+  }
 
 function HomePage() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -30,7 +50,24 @@ function HomePage() {
             if (ctx === null) {
                 console.error("Cannot get 2d context from canvas!");
             } else {
-                draw(ctx)
+
+                const loop = () => {
+                    // blue rect
+                    // clear the canvas
+                    ctx.clearRect(0, 0, 150, 150);
+                    const time = new Date();
+                
+                    const angle =((2 * Math.PI) / 6) * time.getSeconds() +
+                    ((2 * Math.PI) / 6000) * time.getMilliseconds();
+                
+                    const square = new Square(25);
+                    const rotatedSquare = transformShape(getRotationMatrix(angle), square);
+                    ctx.fill(getPath(75, 75, rotatedSquare));
+                    window.requestAnimationFrame(loop);
+                }
+                
+                window.requestAnimationFrame(loop);
+                
             }
         }
       }, [canvasRef])      
